08ebd24854c5bfe09edf17c6bd38283daeccf5d8
ztc-git <56212732+ztc-git@users.noreply.github.com>
2020-04-11 12:47:26 +0800

    跨域

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,7 +9,7 @@ func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
 
-		c.Header("Access-Control-Allow-Origin", "https://island.hellosun.net.cn")
+		c.Header("Access-Control-Allow-Origin", "http://island.hellosun.net.cn")
 		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
